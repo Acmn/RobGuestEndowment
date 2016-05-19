bf8cb0c4303e26983e1d8beddaf6ebc18d0ffb82
Ann McCartney <ann@acmn.com.au>
2016-05-19 16:46:28 +1000

    2016 Updates

--- sidebar.php
+++ sidebar.php
@@ -13,12 +13,12 @@
  <div class="small-12 medium-6 large-12 columns">
     <div class="widget">
       <header>
-        <h2 class="h5">2015 Applications now open</h2>
+        <h2 class="h5">2016 Applications Opening Soon</h2>
       </header>
         <a href="<?php bloginfo('url'); ?>/index.php/awards/rob-guest-endowment/" class="alt-button">Rob Guest Endowment Award</a>
         <a href="<?php bloginfo('url'); ?>/index.php/awards/musician-award/" class="alt-button">The Musician Award</a>
       <a href="<?php bloginfo('url'); ?>/index.php/awards/technical-award/" class="alt-button">The Technical Award</a>
-      <a href="<?php bloginfo('url'); ?>/index.php/awards/2015-awards/" class="button expand">Apply Now</a>
+      <a href="<?php bloginfo('url'); ?>/index.php/awards/2016-awards/" class="button expand">Apply Now</a>
     </div>
   </div>
 
